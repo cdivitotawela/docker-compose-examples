2afaaa8d012dd7a21d83333739c27eaf92bb394f
Chaminda Divitotawela <cdivitotawela@gmail.com>
2024-07-02 15:07:52 +1000

    Initial configuration of Besu and Teku compose stack

---
+++ 03-besu-teku/init.sh
@@ -0,0 +1,22 @@
+#/bin/sh
+
+log_info() {
+  echo "$1"
+}
+
+log_error() {
+  log_info "ERROR: $1"
+  exit 1
+}
+
+# Validations
+test -n $BESU_CONFIG_FILE || log_error "Missing value for environment variable BESU_CONFIG_FILE"
+test -n $JWT_SECRET_FILE || log_error "Missing value for environment variable JWT_SECRET_FILE"
+
+echo "Generating JWT secret if not available"
+test -s $JWT_SECRET_FILE || openssl rand -hex 32 | tr -d "\n" > $JWT_SECRET_FILE
+
+PUBLIC_IP=$(curl https://ifconfig.me)
+log_info "Public IP $PUBLIC_IP"
+test -n $PUBLIC_IP || log_error "Public ip is empty"
+echo "p2p-host=$PUBLIC_IP" > $BESU_CONFIG_FILE
--- README.md
+++ README.md
@@ -8,3 +8,4 @@ This repoisitory contains some examples of docker-compose stacks.
 |-------------------------------------------|---------------------------------------------------------------------------|
 | [01-inline-command](./01-inline-command/) | Running a command in a loop inline compose file                           |
 | [02-loki-stack](./02-loki-stack/)         | Basic Grafana Loki stack for testing with docker-driver                   |
+| [03-besu-teku](./03-besu-teku/)           | Starting Besu and Teku in Sepolia testnet                                 |
